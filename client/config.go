da79a45902b8710454f5f6745e696d1b7bab9988
xpwu <diodexp@foxmail.com>
2026-08-03 22:52:00 +0800

    remove config.id

--- client/config.go
+++ client/config.go
@@ -12,14 +12,3 @@ type Config struct {
 	Password string `conf:"passwd"`
 	MaxConn  uint64 `conf:"maxconn"`
 }
-
-//func (c *Config) Id() string {
-//	sha := sha1.New()
-//	sha.Write([]byte(c.User))
-//	sha.Write([]byte(c.Password))
-//	sha.Write([]byte(c.URI))
-//	buf := make([]byte, 8)
-//	binary.BigEndian.PutUint64(buf, c.MaxConn)
-//	sha.Write(buf)
-//	return hex.EncodeToString(sha.Sum([]byte{}))
-//}
